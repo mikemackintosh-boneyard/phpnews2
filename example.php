999888334bbdc1e032099c23443de154f7fa295c
Mike Mackintosh <mike.mackintosh@angrystatic.com>
2011-09-14 17:04:00 -0300

    Edited example.php via GitHub

--- example.php
+++ example.php
@@ -1,1 +1,19 @@
 <?php
+echo 'Hello!';
+$class = new (int) Class($hello, __class__, array('More' => 'Bar'));
+
+define('IN', 'OUT');
+
+echo IN;
+$a[]='d;';
+
+public function var(){
+  
+  $this->thatS!;
+}
+
+
+/******************
+******************/
+// dsfa sdfsdf
+# dfsdfasdfsdfdsfsdfsafs
